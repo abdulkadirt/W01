ceff144db577630711b4499d0707f050abac722d
Abdulkadir Temizoğlu <tmzglbdlkdr@gmail.com>
2024-03-03 08:24:44 +0300

    registration added , data transportation client to server is done and server to client with viewbag is done

---
+++ W02/Controllers/RegistrationsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace W02.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About(int id) 
+        {
+
+            if (id ==1 )
+            {
+                 ViewBag.id = id;
+                ViewBag.firstName = "Alican";
+                ViewBag.lastName = "Cesur";
+                ViewBag.department = "Math Eng.";
+            }
+            else if (id ==2)
+            {
+                ViewBag.id = id;
+                ViewBag.firstName = "Murtaza";
+                ViewBag.lastName = "Kızıl";
+                ViewBag.department = "Math Eng.";
+            }
+            else
+            {
+                {
+                    ViewBag.id = -1;
+                    ViewBag.firstName = "No data found";
+                    ViewBag.lastName = "No data found";
+                    ViewBag.department = "No data found";
+
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create(int id, string firstName , string LastName , string department)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ W02/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace W02.Models
+{
+    public class Student
+    {
+
+        public int Id {  get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Departmet { get; set; }
+
+
+    }
+}
